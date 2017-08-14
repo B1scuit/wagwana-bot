04969b271d8e759c0362c7ea3e9a36fc4ff93550
Peter Holt <peterholt@b1scuit.local>
2017-08-14 19:40:28 +0100

    Added compliment bot.

---
+++ actions/ComplimentAction.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+  main: function(args, bot){
+    var request = require('request');
+
+    if(args.userID == '262995255660380161'){
+      request('https://insult.mattbas.org/api/insult.txt?who=' + args.user, function(err, res, body){
+        bot.sendMessage({
+          to: args.channelID,
+          message : body,
+          tts: false
+        });
+
+        console.log('[complimentAction]: Generated compliment:' + body);
+
+      });
+
+      return true;
+    } else {
+      request('http://compliment-api.herokuapp.com', function(err, res, body){
+        console.log(body);
+        bot.sendMessage({
+          to: args.channelID,
+          message : '@' + user + ' ' + body,
+          tts: false
+        });
+
+        console.log('[complimentAction]: Generated compliment:' + body);
+
+      });
+
+      return true;
+    }
+  }
+
+}
