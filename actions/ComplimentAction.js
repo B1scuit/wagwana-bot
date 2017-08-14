ca75a0ad8ce1c5a20f22488456c4ecd30d1db642
Peter Holt <peterholt@b1scuit.local>
2017-08-14 19:49:13 +0100

    Added comments

--- actions/ComplimentAction.js
+++ actions/ComplimentAction.js
@@ -3,7 +3,7 @@ module.exports = {
   main: function(args, bot){
     var request = require('request');
 
-    if(args.userID == '262995255660380161'){
+    if(args.userID == '262995255660380161'){ // If its sam, run the insulter anyway
       request('https://insult.mattbas.org/api/insult.txt?who=' + args.user, function(err, res, body){
         bot.sendMessage({
           to: args.channelID,
@@ -16,7 +16,7 @@ module.exports = {
       });
 
       return true;
-    } else {
+    } else { // Otherwise compliment away!
       request('http://compliment-api.herokuapp.com', function(err, res, body){
 
         bot.sendMessage({
